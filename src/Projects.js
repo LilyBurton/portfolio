6fecea9fcd8cb28b42e4a4e3c51336649b68e1a5
LilyBurton <lily.burton1992@gmail.com>
2024-10-14 17:25:20 +0100

    Updated the layout on the project section

--- src/Projects.js
+++ src/Projects.js
@@ -5,109 +5,144 @@ import KanbanBoard from './components/KanbanBoard.png';
 import KanbanBoardVideo from './components/KanbanBoardVideo.mp4';
 import FlavourFinderScreenshot from './components/FlavourFinderScreenshot.png';
 import FlavourFinderVideo from './components/FlavourFinderVideo.mp4';
+import ReactBlog from './components/ReactBlog.png';
+import ReactBlogVideo from './components/ReactBlogVideo.mp4';
 
 const Projects = () => {
-    // State to track which video is currently active
     const [activeVideo, setActiveVideo] = useState(null);
 
-    // Function to handle image clicks and set the active video
     const handleImageClick = (videoType) => {
         setActiveVideo(videoType);
     };
 
-    // Function to handle video click and close the video
     const handleVideoClick = () => {
         setActiveVideo(null);
     };
 
     return (
         <div className='project-container'>
-            {/* Grocery Project */}
-            {activeVideo !== 'kanban' && activeVideo !== 'flavourfinder' && (
-                <>
-                    {!activeVideo ? (
-                        <img
-                            className="grocery-screenshot"
-                            src={Groceries}
-                            alt='Screenshot of Grocery list'
-                            onClick={() => handleImageClick('grocery')}
-                        />
-                    ) : null}
+            <div className='top-projects'>
+                {/* Grocery Project */}
+                {activeVideo !== 'kanban' && activeVideo !== 'flavourfinder' && activeVideo !== 'react-blog' && (
+                    <>
+                        {!activeVideo ? (
+                            <img
+                                className="grocery-screenshot"
+                                src={Groceries}
+                                alt='Screenshot of Grocery list'
+                                onClick={() => handleImageClick('grocery')}
+                            />
+                        ) : null}
 
-                    {activeVideo === 'grocery' && (
-                        <div className='video-container'>
-                            <video
-                                width='600'
-                                controls
-                                autoPlay
-                                onClick={handleVideoClick}
-                            >
-                                <source src={GroceriesVideo} type='video/mp4' />
-                                Your browser does not support the video tag.
-                            </video>
-                        </div>
-                    )}
-                </>
-            )}
+                        {activeVideo === 'grocery' && (
+                            <div className='video-container'>
+                                <video
+                                    width='600'
+                                    controls
+                                    autoPlay
+                                    onClick={handleVideoClick}
+                                >
+                                    <source src={GroceriesVideo} type='video/mp4' />
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                        )}
+                    </>
+                )}
 
-            {/* Kanban Board Project */}
-            {activeVideo !== 'grocery' && activeVideo !== 'flavourfinder' && (
-                <>
-                    {!activeVideo ? (
-                        <img
-                            className="kanban-screenshot"
-                            src={KanbanBoard}
-                            alt='Screenshot of Kanban Board'
-                            onClick={() => handleImageClick('kanban')}
-                        />
-                    ) : null}
+                {/* Kanban Board Project */}
+                {activeVideo !== 'grocery' && activeVideo !== 'flavourfinder' && activeVideo !== 'react-blog' && (
+                    <>
+                        {!activeVideo ? (
+                            <img
+                                className="kanban-screenshot"
+                                src={KanbanBoard}
+                                alt='Screenshot of Kanban Board'
+                                onClick={() => handleImageClick('kanban')}
+                            />
+                        ) : null}
 
-                    {activeVideo === 'kanban' && (
-                        <div className='kanban-video-container'>
-                            <video
-                                width='600'
-                                controls
-                                autoPlay
-                                onClick={handleVideoClick}
-                            >
-                                <source src={KanbanBoardVideo} type='video/mp4' />
-                                Your browser does not support the video tag.
-                            </video>
-                        </div>
-                    )}
-                </>
-            )}
+                        {activeVideo === 'kanban' && (
+                            <div className='kanban-video-container'>
+                                <video
+                                    width='600'
+                                    controls
+                                    autoPlay
+                                    onClick={handleVideoClick}
+                                >
+                                    <source src={KanbanBoardVideo} type='video/mp4' />
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                        )}
+                    </>
+                )}
+            </div>
 
-            {/* Recipe Generator */}
-            {activeVideo !== 'grocery' && activeVideo !== 'kanban' && (
-                <>
-                    {!activeVideo ? (
-                        <img
-                            className='flavour-finder-screenshot'
-                            src={FlavourFinderScreenshot}
-                            alt='Screenshot of FlavourFinder'
-                            onClick={() => handleImageClick('flavourfinder')}
-                        />
-                    ) : null}
+            <div className='bottom-projects'>
+                {/* Flavour Finder Project */}
+                {activeVideo !== 'grocery' && activeVideo !== 'kanban' && activeVideo !== 'react-blog' && (
+                    <>
+                        {!activeVideo ? (
+                            <img
+                                className='flavour-finder-screenshot'
+                                src={FlavourFinderScreenshot}
+                                alt='Screenshot of FlavourFinder'
+                                onClick={() => handleImageClick('flavourfinder')}
+                            />
+                        ) : null}
 
-                    {activeVideo === 'flavourfinder' && (
-                        <div className='flavour-finder-video'>
-                            <video
-                                width='600'
-                                controls
-                                autoPlay
-                                onClick={handleVideoClick}
-                            >
-                                <source src={FlavourFinderVideo} type='video/mp4' />
-                                Your browser does not support the video tag.
-                            </video>
-                        </div>
-                    )}
-                </>
-            )}
+                        {activeVideo === 'flavourfinder' && (
+                            <div className='flavour-finder-video'>
+                                <video
+                                    width='600'
+                                    controls
+                                    autoPlay
+                                    onClick={handleVideoClick}
+                                >
+                                    <source src={FlavourFinderVideo} type='video/mp4' />
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                        )}
+                    </>
+                )}
+
+                {/* React Blog */}
+                {activeVideo !== 'grocery' && activeVideo !== 'kanban' && activeVideo !== 'flavourfinder' && (
+                    <>
+                        {!activeVideo ? (
+                            <img
+                                className='react-blog-screenshot'
+                                src={ReactBlog}
+                                alt='Screenshot of React Blog'
+                                onClick={() => handleImageClick('react-blog')}
+                            />
+                        ) : null}
+
+                        {activeVideo === 'react-blog' && (
+                            <div className='react-blog-video'>
+                                <video
+                                    width='600'
+                                    controls
+                                    autoPlay
+                                    onClick={handleVideoClick}
+                                >
+                                    <source src={ReactBlogVideo} type='video/mp4' />
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>
+                        )}
+                    </>
+                )}
+            </div>
         </div>
     );
 };
 
 export default Projects;
+
+
+
+
 
