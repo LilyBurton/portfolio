5865153d628eab0afb46f2637e820be16b570cf0
LilyBurton <lily.burton1992@gmail.com>
2024-10-21 17:42:16 +0100

    Using Web 3 Forms to connect the email to the form

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,27 +1,56 @@
 import React from 'react';
 
 const Contacts = () => {
-    return(
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "53e8d146-d672-4d34-b9a4-742f1063796d");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          console.log("Success", res);
+        }
+      };
+    
+
+    return (
         <div>
             <section className='contact'>
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className='input-box'>
                         <label>Full Name</label>
-                        <input className='field-box' placeholder='Enter your name ' required />
+                        <input className='field-box' placeholder='Enter your name ' name='name' required />
                     </div>
                     <div className='input-box'>
                         <label>Email</label>
-                        <input className='field-box' placeholder='Enter your email ' required />
+                        <input className='field-box' placeholder='Enter your email ' name='email' required />
                     </div>
                     <div className='input-box'>
                         <label>Your Message</label>
-                        <textarea name="" id="" className="field-box big" placeholder="Enter your message " required></textarea>
+                        <textarea name='message' className="field-box big" placeholder='Enter your message ' required></textarea>
                     </div>
                     <button type="submit">Send Message</button>
                 </form>
             </section>
         </div>
-    )
-}
+    );
+};
 
 export default Contacts;
+
+
+
+
+
