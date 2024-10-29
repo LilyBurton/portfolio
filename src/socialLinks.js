63f520d97c386bda824db0819977dfcb9a1baa75
LilyBurton <lily.burton1992@gmail.com>
2024-10-29 16:08:17 +0000

    open links in a new tab

--- src/socialLinks.js
+++ src/socialLinks.js
@@ -6,10 +6,10 @@ const SocialLinks = () => {
     return (
         <div className='social-container'>
             <div className='social-links-icons'>
-                <a href='https://www.linkedin.com/in/lily-burton-7a8826a9/'>
+                <a href='https://www.linkedin.com/in/lily-burton-7a8826a9/' target='_blank' rel="noopener noreferrer">
                     <FaLinkedin className="linkedin-icon" size={100} />
                 </a>
-                <a href='https://github.com/LilyBurton'>
+                <a href='https://github.com/LilyBurton' target='_blank' rel="noopener noreferrer">
                     <FaGithub className="github-icon" size={100} />
                 </a>
             </div>
