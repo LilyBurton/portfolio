d4ec0dff4fe789af142f910fa98521d8d87a5ea9
LilyBurton <lily.burton1992@gmail.com>
2024-11-19 17:04:12 +0000

    Readjusting website to be suitable for tablet size

--- src/Projects.js
+++ src/Projects.js
@@ -46,7 +46,6 @@ const Projects = () => {
                                     Your browser does not support the video tag.
                                 </video>
                                 <p className='Paragraph'>I used React and CSS to set up the application where you can check and cross out the items, add new items, delete items and find the object on the list by using the search bar. I also use json database so the user can keep the changes on the list.</p>
-                                <p className='click-instruction'>CLICK on the video to go back</p>
                             </div>
                         )}
                     </>
@@ -76,7 +75,6 @@ const Projects = () => {
                                     Your browser does not support the video tag.
                                 </video>
                                 <p className='paragraph'>The kanban board has been made through React where I have to import 'Draggable' and 'Droppable' from 'react-beautiful-dnd' so the users would be able to drag and drop tasks between columns. I used socket.io so it will be able to save data under the id of a user's username.</p>
-                                <p className='click-instruction'>CLICK on the video to go back</p>
                             </div>
                         )}
                     </>
@@ -109,7 +107,6 @@ const Projects = () => {
                                 </video>
                                 <p className='paragraph'>Flavour Finder is a recipe generator where the user can generate recipes through ingredients, nationality, meal type and dietary requirements. We want to make it user friendly as possible for both users and non users where they can still try out the recipes generator before signing up and when they do they will have the ability to save the recipes. We use Mongo.db to be able to save the users details in mongo.db and fetch data from the api to be able to generate different recipes. What I love about this generator is it makes the dietry mandatory. For example, if you dietary is vegan but you put milk as an ingredient it will make sure to add a vegan friendly milk within the recipe.</p>
                                 <p>Link to the Recipe Generator! <a href='https://flavour-finder.onrender.com/' target='_blank' rel="noopener noreferrer">Click here!</a></p>
-                                <p className='click-instruction'>CLICK on the video to go back</p>
                             </div>
                         )}
                     </>
@@ -139,15 +136,11 @@ const Projects = () => {
                                     Your browser does not support the video tag.
                                 </video>
                                 <p className='paragraph'>The React Blog was made from React where I was focusing on using the CRUD method so the users will be able to create, read, update and delete posts along with using the json database serve to be able to keep the changes. I was also trying out axios to understand fetching data from the json.db.</p>
-                                <p className='click-instruction'>CLICK on the video to go back</p>
                             </div>
                         )}
                     </>
                 )}
-            </div>
-            {activeVideo !== 'grocery' && activeVideo !== 'kanban' && activeVideo !== 'flavourfinder' && activeVideo !== 'react-blog' && (
-                <p className='click-instruction'>CLICK on the image to start a video</p>
-            )}  
+            </div> 
         </div>
     );
 };
